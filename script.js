81bcca0c3e8a25f334ccf0e395cfddd7d894b8b1
Luciano Garriga <lucianomgarriga@gmail.com>
2023-01-25 17:34:10 -0300

    Body bg color mod

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const body = document.querySelector('body');
 toggle.addEventListener('click', function() {
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
-        body.style.background = ('#ced4da');
+        body.style.background = '#ced4da';
         body.style.color = 'black';
         body.style.transition = '2s';
     } else {
-        body.style.background = ('#212529'); 
+        body.style.background = '#212529'; 
         body.style.color = 'white';
         body.style.transition = '2s';
     }
